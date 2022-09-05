a46d62308f83bfd73179fc4174a8e2524ebff8af
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-05 21:12:00 +0200

    remove disable sandbox

--- xcodeinstall.rb
+++ xcodeinstall.rb
@@ -17,7 +17,6 @@ class Xcodeinstall < Formula
   def install
     system "swift", "build",
         "--configuration", "release",
-        "--disable-sandbox"
     bin.install '.build/release/xcodeinstall'
     bin.install 'libSwiftToolsSupport.dylib'
   end
