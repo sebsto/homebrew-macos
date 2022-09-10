a593f94fff29024524753e7ca3d888502e9eff6a
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-10 20:24:50 +0200

    update for v0.3

--- xcodeinstall.rb
+++ xcodeinstall.rb
@@ -2,27 +2,21 @@
 # https://betterprogramming.pub/a-step-by-step-guide-to-create-homebrew-taps-from-github-repos-f33d3755ba74
 # https://medium.com/@mxcl/maintaining-a-homebrew-tap-for-swift-projects-7287ed379324
 
-#
-# To compute the SHA :
-# ➜  mac shasum -a 256 v0.2.tar.gz
-# fe1c01175493bacd9f8523a475c9d028fb5d8cfb9fd199f6b80c7d3d356cdb58  v0.2.tar.gz
-#
-
 class Xcodeinstall < Formula
-  desc "This is a command-line tool to download and install Apple's Xcode"
-  homepage "https://github.com/sebsto/xcodeinstall"
-  url "https://github.com/sebsto/xcodeinstall/archive/refs/tags/v0.2.tar.gz"
-  sha256 "fe1c01175493bacd9f8523a475c9d028fb5d8cfb9fd199f6b80c7d3d356cdb58"
-  license "Apache-2.0"
-
-  def install
-    system "swift", "build",
-        "--configuration", "release",
-        "--disable-sandbox"
-    bin.install ".build/release/xcodeinstall"
+    desc "This is a command-line tool to download and install Apple's Xcode"
+    homepage "https://github.com/sebsto/xcodeinstall"
+    url "https://github.com/sebsto/xcodeinstall/archive/refs/tags/v0.3.tar.gz"
+    sha256 "127f1b7a5216e464b97f5f59f868ef50eee044146297c6da58ca27d957f1a5a7"
+    license "Apache-2.0"
+  
+    def install
+      system "swift", "build",
+          "--configuration", "release",
+          "--disable-sandbox"
+      bin.install ".build/release/xcodeinstall"
+    end
+  
+    test do
+      assert_equal version.to_s, shell_output("#{bin}/xcodeinstall --version").chomp
+    end
   end
-
-  test do
-    assert_equal version.to_s, shell_output("#{bin}/xcodeinstall --version").chomp
-  end
-end
