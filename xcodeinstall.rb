187628c483afbf7d1bc004a3e04a946efc680b97
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-11 15:49:39 +0200

    update for v0.3

--- xcodeinstall.rb
+++ xcodeinstall.rb
@@ -9,6 +9,13 @@ class Xcodeinstall < Formula
   sha256 "752662290033f945c9bc6f9375f6964a4881844efd24a1beada560b312f5f9d7"
   license "Apache-2.0"
 
+  # insert bottle definition here
+bottle do
+  root_url "https://github.com/sebsto/xcodeinstall/releases/download/v"
+  sha256 cellar: :any_skip_relocation, arm64_monterey: "107a934f43d092c9ced521e088b45f4e4696c292b3b044b687587e74aeca3bb7"
+  sha256 cellar: :any_skip_relocation, monterey:       "107a934f43d092c9ced521e088b45f4e4696c292b3b044b687587e74aeca3bb7"
+end
+
   def install
     system "./scripts/build_fat_binary.sh"
     bin.install ".build/apple/Products/Release/xcodeinstall"
