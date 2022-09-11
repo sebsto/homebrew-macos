b465e822ee03beb7360bb02f2d1d4002cbffca58
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-11 17:29:47 +0200

    update for v0.4

--- xcodeinstall.rb
+++ xcodeinstall.rb
@@ -5,16 +5,11 @@
 class Xcodeinstall < Formula
   desc "This is a command-line tool to download and install Apple's Xcode"
   homepage "https://github.com/sebsto/xcodeinstall"
-  url "https://github.com/sebsto/xcodeinstall/archive/refs/tags/v0.3.tar.gz"
-  sha256 "752662290033f945c9bc6f9375f6964a4881844efd24a1beada560b312f5f9d7"
+  url "https://github.com/sebsto/xcodeinstall/archive/refs/tags/v0.4.tar.gz"
+  sha256 "c0077ff2c060423f8ffd94656da14a740fcaf21eb3d37c5b9e8f4761a2f2bdd8"
   license "Apache-2.0"
 
   # insert bottle definition here
-  bottle do
-    root_url "https://github.com/sebsto/xcodeinstall/releases/download/v0.3"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a02ac1e31d0c79441f54865996da4a2e578204cdba10828049ca9cc75dc6cbb2"
-    sha256 cellar: :any_skip_relocation, monterey: "a02ac1e31d0c79441f54865996da4a2e578204cdba10828049ca9cc75dc6cbb2"
-  end
 
   def install
     system "./scripts/build_fat_binary.sh"
