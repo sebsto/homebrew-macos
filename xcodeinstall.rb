9e355d883025d68b984814fa44ac3d5fa00a17da
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-09-25 17:41:01 +0200

    update for v0.14.0

--- xcodeinstall.rb
+++ xcodeinstall.rb
@@ -10,6 +10,15 @@ class Xcodeinstall < Formula
   license "Apache-2.0"
 
   # insert bottle definition here
+  bottle do
+    root_url "https://github.com/sebsto/xcodeinstall/releases/download/v0.14.0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9e599ff8b1091f5761c40ada4c24382c2858ed7de425c0beb8b1e05f4e8878fa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "9e599ff8b1091f5761c40ada4c24382c2858ed7de425c0beb8b1e05f4e8878fa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9e599ff8b1091f5761c40ada4c24382c2858ed7de425c0beb8b1e05f4e8878fa"
+    sha256 cellar: :any_skip_relocation, ventura: "9e599ff8b1091f5761c40ada4c24382c2858ed7de425c0beb8b1e05f4e8878fa"
+    sha256 cellar: :any_skip_relocation, sonoma: "9e599ff8b1091f5761c40ada4c24382c2858ed7de425c0beb8b1e05f4e8878fa"
+    sha256 cellar: :any_skip_relocation, sequoia: "9e599ff8b1091f5761c40ada4c24382c2858ed7de425c0beb8b1e05f4e8878fa"
+  end
 
   def install
     system "./scripts/deploy/build_fat_binary.sh"
