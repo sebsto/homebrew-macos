29d275fd22683bb8fd6caa7afbe21112f9f5d738
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2026-02-26 18:08:08 +0100

    Update xcodeinstall to 0.20.0

--- xcodeinstall.rb
+++ xcodeinstall.rb
@@ -1,20 +1,20 @@
 class Xcodeinstall < Formula
   desc "This is a command-line tool to download and install Apple's Xcode"
   homepage "https://github.com/sebsto/xcodeinstall"
-  url "https://github.com/sebsto/xcodeinstall/archive/refs/tags/v0.19.0.tar.gz"
-  sha256 "837e116b5d0936f240bb8abcf50f2d23543679030e0c38b6d5bdac58823511aa"
+  url "https://github.com/sebsto/xcodeinstall/archive/refs/tags/v0.20.0.tar.gz"
+  sha256 "7ec06e79fcca3e0054d298beded6815071701e091ce111dbc832c9fe0004fda4"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/sebsto/xcodeinstall/releases/download/v0.19.0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "41104b00275c6050b293c6c0389eabe5f138b533559c37a48d0af6a4466befc2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "41104b00275c6050b293c6c0389eabe5f138b533559c37a48d0af6a4466befc2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "41104b00275c6050b293c6c0389eabe5f138b533559c37a48d0af6a4466befc2"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "41104b00275c6050b293c6c0389eabe5f138b533559c37a48d0af6a4466befc2"
-    sha256 cellar: :any_skip_relocation, ventura: "41104b00275c6050b293c6c0389eabe5f138b533559c37a48d0af6a4466befc2"
-    sha256 cellar: :any_skip_relocation, sonoma: "41104b00275c6050b293c6c0389eabe5f138b533559c37a48d0af6a4466befc2"
-    sha256 cellar: :any_skip_relocation, sequoia: "41104b00275c6050b293c6c0389eabe5f138b533559c37a48d0af6a4466befc2"
-    sha256 cellar: :any_skip_relocation, tahoe: "41104b00275c6050b293c6c0389eabe5f138b533559c37a48d0af6a4466befc2"
+    root_url "https://github.com/sebsto/xcodeinstall/releases/download/v0.20.0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "56d565fc25f3ace446a76ae8f8ccac38dfebbc03a664c838f6d8b2bcb227a6e4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "56d565fc25f3ace446a76ae8f8ccac38dfebbc03a664c838f6d8b2bcb227a6e4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "56d565fc25f3ace446a76ae8f8ccac38dfebbc03a664c838f6d8b2bcb227a6e4"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "56d565fc25f3ace446a76ae8f8ccac38dfebbc03a664c838f6d8b2bcb227a6e4"
+    sha256 cellar: :any_skip_relocation, ventura: "56d565fc25f3ace446a76ae8f8ccac38dfebbc03a664c838f6d8b2bcb227a6e4"
+    sha256 cellar: :any_skip_relocation, sonoma: "56d565fc25f3ace446a76ae8f8ccac38dfebbc03a664c838f6d8b2bcb227a6e4"
+    sha256 cellar: :any_skip_relocation, sequoia: "56d565fc25f3ace446a76ae8f8ccac38dfebbc03a664c838f6d8b2bcb227a6e4"
+    sha256 cellar: :any_skip_relocation, tahoe: "56d565fc25f3ace446a76ae8f8ccac38dfebbc03a664c838f6d8b2bcb227a6e4"
   end
 
   def install
