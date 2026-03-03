bea7de0d05572eeb8b09dd5444ffa8dd613c3fcb
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2026-03-03 23:13:34 +0100

    Update wispr to 1.0.0

--- Casks/wispr.rb
+++ Casks/wispr.rb
@@ -1,6 +1,6 @@
 cask "wispr" do
   version "1.0.0"
-  sha256 "22f082759976e807e21fdb65269439f575fdae79b8c86cb39043cdfee5ef67f6"
+  sha256 "4c6c741dfb9e595e1dd915394f11a6770347d121c798a4144667228aea15d1a6"
 
   url "https://github.com/sebsto/wispr/releases/download/v1.0.0/wispr-1.0.0.zip"
   name "Wispr"
