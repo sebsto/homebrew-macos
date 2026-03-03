8405ae2ba8ac32dab680a319a638f8c9d1745d52
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2026-03-03 23:29:51 +0100

    Fix wispr SHA256 to match current v1.0.0 release

--- Casks/wispr.rb
+++ Casks/wispr.rb
@@ -1,6 +1,6 @@
 cask "wispr" do
   version "1.0.0"
-  sha256 "4c6c741dfb9e595e1dd915394f11a6770347d121c798a4144667228aea15d1a6"
+  sha256 "7008e8b47e2916612997a2afe63fd9d396c9983a76376de417bfa7ea4ee48058"
 
   url "https://github.com/sebsto/wispr/releases/download/v1.0.0/wispr-1.0.0.zip"
   name "Wispr"
