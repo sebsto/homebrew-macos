2bc3f539b3fa6a785a45448623698d96f45718e3
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2026-03-24 11:21:53 +0100

    Update wispr to 1.5.0

--- Casks/wispr.rb
+++ Casks/wispr.rb
@@ -1,8 +1,8 @@
 cask "wispr" do
-  version "1.4.0"
-  sha256 "9e2f9fe5899f3c936706c5c8b29dd417b3e1b36ad9749b9a682b9fbcbe4907c5"
+  version "1.5.0"
+  sha256 "1e66dc28596ba43e75b1b857e68ad8f2c1d6f9288c0ab6ce34e0fa66b98564bf"
 
-  url "https://github.com/sebsto/wispr/releases/download/v1.4.0/wispr-1.4.0.zip"
+  url "https://github.com/sebsto/wispr/releases/download/v1.5.0/wispr-1.5.0.zip"
   name "Wispr"
   desc "Local speech-to-text transcription powered by OpenAI Whisper"
   homepage "https://github.com/sebsto/wispr"
