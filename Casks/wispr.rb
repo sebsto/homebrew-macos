bd83dac30d376e589c2b599c7a6374d0e573ec5e
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2026-03-29 12:58:04 +0200

    Revert "Update wispr to 1.7.0"
    This reverts commit 5cc20ab6ce141c1d7d5befa778b4c395296483bb.

--- Casks/wispr.rb
+++ Casks/wispr.rb
@@ -1,8 +1,8 @@
 cask "wispr" do
-  version "1.7.0"
-  sha256 "2ca08a3a219868fb3a33c3bffa8d52722ad52b87a478f8388d8b47b9c1a5f0c4"
+  version "1.6.0"
+  sha256 "ce9659ed89b73055e3446d5c11cc9e75065305a748716e252cf8c900f537d1dc"
 
-  url "https://github.com/sebsto/wispr/releases/download/v1.7.0/wispr-1.7.0.zip"
+  url "https://github.com/sebsto/wispr/releases/download/v1.6.0/wispr-1.6.0.zip"
   name "Wispr"
   desc "Local speech-to-text transcription powered by OpenAI Whisper"
   homepage "https://github.com/sebsto/wispr"
