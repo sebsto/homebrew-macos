dd1b2f8b391c3ea84d127e8d5426227a1b32d1ae
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2026-06-24 20:36:14 +0200

    Revert "Update wispr to 1.11.0"
    This reverts commit eb418373d6def87f37d95231642fa837a6385129.

--- Casks/wispr.rb
+++ Casks/wispr.rb
@@ -1,8 +1,8 @@
 cask "wispr" do
-  version "1.11.0"
-  sha256 "d24e45f61eca813a2319f64d50b7c548fb920994454b019cc9e981cef38bbafb"
+  version "1.10.1"
+  sha256 "98647dc7a9612323dc66904cb90ab5c2b7008114bd17c1339182f23695edd55a"
 
-  url "https://github.com/sebsto/wispr/releases/download/v1.11.0/wispr-1.11.0.zip"
+  url "https://github.com/sebsto/wispr/releases/download/v1.10.1/wispr-1.10.1.zip"
   name "Wispr"
   desc "Local speech-to-text transcription powered by OpenAI Whisper"
   homepage "https://github.com/sebsto/wispr"
